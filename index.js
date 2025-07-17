e7b323146bbbb7d3bdf9d7c0f2fdb71b8361ea4a
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2025-07-16 22:23:16 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+if (process.argv.length != 3) {
+  console.error("Usage: node index.js <dockerPath>");
+  process.exit(1);
+}
+
+const dockerPath = process.argv[2];
+const main = require(__dirname + "/main.js");
+
+main(dockerPath);
