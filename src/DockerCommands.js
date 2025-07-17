6cd2d066f4f73366b003f2255611029f52768c29
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2025-07-17 15:49:44 -0300

    Update DockerCommands.js

--- src/DockerCommands.js
+++ src/DockerCommands.js
@@ -3,5 +3,9 @@ const { getImageNameFromPath } = require(__dirname + '/Utils.js');
 
 module.exports.buildImage = async function (buildPath) {
   const imageName = getImageNameFromPath(buildPath);
+  if (!imageName) {
+    throw new Error(`Could not determine image name from path: ${buildPath}`);
+  }
+  console.log(`Building Docker image: ${imageName} from path: ${buildPath}`);
   return System.execAsync(`docker build -t ${imageName} ${buildPath}`);
 };
